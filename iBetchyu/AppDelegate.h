f6d2d2f00a2da041ced3e42aa892b04e9f4388de
Betchyu Computer <Betchyu@Betchyus-MacBook-Pro.local>
2013-11-16 10:49:20 -0500

    Initial Commit

---
+++ iBetchyu/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  iBetchyu
+//
+//  Created by Betchyu Computer on 11/16/13.
+//  Copyright (c) 2013 Betchyu Computer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iBetchyu/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  iBetchyu
+//
+//  Created by Betchyu Computer on 11/16/13.
+//  Copyright (c) 2013 Betchyu Computer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
